d302658d2faf5261de396da3a6c7b9ca358f2440
Larissa O Ramos <larissadeoliveiraramos@hotmail.com>
2024-11-21 18:42:47 -0300

    Created User class

---
+++ src/main/java/com/boxoffice/ticketmanager/entity/User/User.java
@@ -0,0 +1,18 @@
+package com.boxoffice.ticketmanager.entity.User;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    private String name;
+    private String email;
+}
